94065d9b6602688163904f6d39829bd467bed84d
suha95 <85480787+suha95@users.noreply.github.com>
2021-11-18 13:31:39 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailFormsComponent } from './forms/detail-forms/detail-forms.component';
+import { FormsComponent } from './forms/forms.component';
+import { AuthGuard } from './guard/auth.guard';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: FormsComponent, canActivate: [AuthGuard] },
+  {
+    path: 'forms/:name/:formId',
+    component: DetailFormsComponent,
+    canActivate: [AuthGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsComponent } from './forms/forms.component';
+import { AddFormsComponent } from './forms/add-forms/add-forms.component';
+import { ListFormsComponent } from './forms/list-forms/list-forms.component';
+import { DetailFormsComponent } from './forms/detail-forms/detail-forms.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './navbar/search/search.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FormsComponent,
+    AddFormsComponent,
+    ListFormsComponent,
+    DetailFormsComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
